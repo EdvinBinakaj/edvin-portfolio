e4aae2d5b6c53e626ef8de6a90da4024ab193e96
EdvinBinakaj <ebi@rasiah.de>
2024-11-09 20:14:39 +0100

    feat(framer-morion): add framer Motion to the website

--- app/components/about-me.tsx
+++ app/components/about-me.tsx
@@ -1,3 +1,5 @@
+import { motion } from "framer-motion";
+
 export const About = () => {
   return (
     <div className="border-b border-neutral-900 pb-4">
@@ -6,12 +8,22 @@ export const About = () => {
       </h2>
 
       <div className="flex flex-wrap">
-        <div className="w-full lg:w-1/2 lg:p-8">
+        <motion.div
+          whileInView={{ opacity: 1, x: 0 }}
+          initial={{ opacity: 0, x: -100 }}
+          transition={{ duration: 0.5 }}
+          className="w-full lg:w-1/2 lg:p-8"
+        >
           <div className="flex items-center justify-center">
             <img src="about.jpg" alt="about" className="rounded-md" />
           </div>
-        </div>
-        <div className="w-full lg:w-1/2">
+        </motion.div>
+        <motion.div
+          whileInView={{ opacity: 1, x: 0 }}
+          initial={{ opacity: 0, x: 100 }}
+          transition={{ duration: 0.5 }}
+          className="w-full lg:w-1/2"
+        >
           <div className="flex justify-center lg:justify-start">
             <p className="my-2 max-w-xl py-6">
               I am a dedicated and versatile full stack developer with a passion
@@ -27,7 +39,7 @@ export const About = () => {
               technologies, and contributing to open-source projects.
             </p>
           </div>
-        </div>
+        </motion.div>
       </div>
     </div>
   );
